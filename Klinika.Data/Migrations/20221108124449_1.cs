456dd1c173d0c231c11105fca4d9ac1d7c52e0cc
Darek_Duda <dduda@interia.pl>
2022-11-09 22:10:50 +0100

    Dodanie classy Rodzaje Wizyt

---
+++ Klinika.Data/Migrations/20221108124449_1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Klinika.Data.Migrations
+{
+    public partial class _1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RodzajeWizyt",
+                columns: table => new
+                {
+                    IdRodzajeWizyty = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nazwa = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RodzajeWizyt", x => x.IdRodzajeWizyty);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RodzajeWizyt");
+        }
+    }
+}
